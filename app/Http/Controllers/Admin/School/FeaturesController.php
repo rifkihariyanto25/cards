25a4d397a20716df766a8ef8934e7f7cb7ad196f
rifkihariyanto25 <hariyantorifki25@gmail.com>
2025-08-14 11:57:21 +0700

    add template folder feature edit create view admin

---
+++ app/Http/Controllers/Admin/School/FeaturesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\School;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FeaturesController extends Controller
+{
+    //
+}
---
+++ app/Models/School/SchoolFeature.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\School;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolFeature extends Model
+{
+    //
+}
